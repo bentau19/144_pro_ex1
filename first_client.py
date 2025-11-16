eb5b745fc743829e24e86146d3dfd30ee31300d8
Yael <yael.doron1@live.biu.ac.il>
2025-11-16 18:09:52 +0200

    delete unnecessary files

--- first_client.py
+++
@@ -1,7 +0,0 @@
-#first part- the client sends the student's names to the server's specific IP and port
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'Ben Tau and Yael Doron', ('192.168.253.123', 12346))
-data, addr = s.recvfrom(1024)
-print(str(data), addr)
-s.close()
